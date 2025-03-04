81d8b56ac0bdc8661c0b67065b9327e9c47f5ab2
Aman <aman1.marve@gmail.com>
2025-03-04 10:04:24 +0530

    axios update

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: "https://hii-chat-backend.vercel.app/api",
+    baseURL: import.meta.env.MODE === "development" ? "https://hii-chat-backend.vercel.app//api" : "/api",
     withCredentials: true,
 })
